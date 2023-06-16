f57a94994553c0d51458233598f1b614977644b2
blonded99 <didls2654@naver.com>
2023-06-17 04:24:08 +0900

    refactor: update 'Movie.swift'
    Renamed 'Movie.swift' file, changed some variable names and types

---
+++ Models/Movie.swift
@@ -0,0 +1,26 @@
+//
+//  Moive.swift
+//  movieinfo-iOS
+//
+//  Created by 김병훈 on 2023/06/16.
+//
+
+import Foundation
+
+struct Movie {
+    var adult: Bool
+    var genres: [String]
+    var imdb_id: Int
+    var original_language: String
+    var overview: String
+    var popularity: Double
+    var poster_path: String
+    var production_companies: [String]
+    var release_date: String
+    var runtime: Double
+    var status: String
+    var title: String
+    var vote_average: Double
+    var vote_count: Double
+    var tmdbId: Double
+}
