eb7a3037942b7499a2b36b84913ee93e69ae1eed
blonded99 <didls2654@naver.com>
2023-06-16 22:17:12 +0900

    Initial Commit

---
+++ movieinfo-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieinfo-iOS
+//
+//  Created by 김병훈 on 2023/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
