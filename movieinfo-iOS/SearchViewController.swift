e3fd816f8b93ca410700976a9d6197d6e349a087
blonded99 <didls2654@naver.com>
2023-06-18 03:40:55 +0900

    feat: implement simple search on the search screen
    Implemented simple search: can search by movie title or actor name, movie title appears when the search button is pressed.

--- movieinfo-iOS/SearchViewController.swift
+++ movieinfo-iOS/SearchViewController.swift
@@ -7,23 +7,65 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var searchTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchResults: [Movie] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
+        performSearch()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func performSearch() {
+        guard let searchText = searchTextField.text else {
+            return
+        }
+        
+        searchResults.removeAll()
+        
+        switch searchTypeSegmentedControl.selectedSegmentIndex {
+        case 0: // MovieTitle
+            searchResults = MovieDataStore.shared.movies.filter { $0.title.lowercased().contains(searchText.lowercased()) }
+        case 1: // ActorName
+            searchResults = MovieDataStore.shared.movies.filter { movie in
+                movie.cast.contains(where: { $0.lowercased().contains(searchText.lowercased()) })
+            }
+        default:
+            break
+        }
+        
+        tableView.reloadData()
     }
-    */
 
+    // MARK: - TableView DataSource Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let movie = searchResults[indexPath.row]
+        
+        // Here you can update the cell with the movie information.
+        cell.textLabel?.text = movie.title
+        
+        return cell
+    }
+    
+    // MARK: - TableView Delegate Methods
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Implement the navigation to Movie Detail Screen
+    }
 }
