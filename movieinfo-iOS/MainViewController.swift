a91d9ffafa0b9f7cc5e483bd0a3c32d922b6f3c2
blonded99 <didls2654@naver.com>
2023-06-17 19:31:53 +0900

    feat: connect each screen to each UIViewController

--- movieinfo-iOS/MainViewController.swift
+++ movieinfo-iOS/MainViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    @IBOutlet var TestLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
