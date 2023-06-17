68792ef598771f825afb67e1fe2af1d292fc0da8
blonded99 <didls2654@naver.com>
2023-06-17 21:13:58 +0900

    feat: implement part of the main screen
    Implemented to display the top 20 movie titles on the main screen in order of popularity.

--- movieinfo-iOS/MainViewController.swift
+++ movieinfo-iOS/MainViewController.swift
@@ -7,12 +7,69 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
-    @IBOutlet var TestLabel: UILabel!
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Instance of CSVLoader to load movies
+    let loader = CSVLoader(filename: "movies_metadata")
+    // An array to hold the loaded movies
+    var movies = [Movie]()
+    // An array to hold the top 20 movies
+    var topMovies = [Movie]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Load movies from CSV file
+        loadMovies()
+        
+        // Sort movies by popularity
+        sortMoviesByPopularity()
+        
+        // Set up tableView
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    // Load movies using the CSVLoader
+    func loadMovies() {
+        movies = loader.load()
+    }
+    
+    // Sort movies by popularity and update topMovies
+    func sortMoviesByPopularity() {
+        let sortedMovies = movies.sorted { $0.popularity > $1.popularity }
+        topMovies = Array(sortedMovies.prefix(20))
+    }
+    
+    // Sort movies by vote count and update topMovies
+    func sortMoviesByVoteCount() {
+        let sortedMovies = movies.sorted { $0.vote_count > $1.vote_count }
+        topMovies = Array(sortedMovies.prefix(20))
+    }
+    
+    // Sort movies by vote average and update topMovies
+    func sortMoviesByVoteAverage() {
+        let sortedMovies = movies.sorted { $0.vote_average > $1.vote_average }
+        topMovies = Array(sortedMovies.prefix(20))
+    }
+    
+    // MARK: - UITableViewDataSource methods
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return topMovies.count
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+
+        if let label = cell.viewWithTag(1) as? UILabel {
+            let movie = topMovies[indexPath.row] // use topMovies here
+            label.text = movie.title
+        }
+
+        return cell
     }
 
 
