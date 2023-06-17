4760c1f2656c10158b6cbdae5d373bbdd5d80b14
blonded99 <didls2654@naver.com>
2023-06-17 22:18:37 +0900

    feat: add more options to sort the movies on the main screen
    Added sorting options based on 'vote_count' and 'vote_average' on the main screen using SegmentedControl.

--- movieinfo-iOS/MainViewController.swift
+++ movieinfo-iOS/MainViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
+    @IBOutlet weak var sortSegmentedControl: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
     
     // Instance of CSVLoader to load movies
@@ -39,33 +39,48 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     // Sort movies by popularity and update topMovies
     func sortMoviesByPopularity() {
-        let sortedMovies = movies.sorted { $0.popularity > $1.popularity }
-        topMovies = Array(sortedMovies.prefix(20))
+        movies.sort { $0.popularity > $1.popularity }
+        topMovies = Array(movies.prefix(20))
     }
-    
+
     // Sort movies by vote count and update topMovies
     func sortMoviesByVoteCount() {
-        let sortedMovies = movies.sorted { $0.vote_count > $1.vote_count }
-        topMovies = Array(sortedMovies.prefix(20))
+        movies.sort { $0.vote_count > $1.vote_count }
+        topMovies = Array(movies.prefix(20))
     }
-    
+
     // Sort movies by vote average and update topMovies
     func sortMoviesByVoteAverage() {
-        let sortedMovies = movies.sorted { $0.vote_average > $1.vote_average }
-        topMovies = Array(sortedMovies.prefix(20))
+        movies.sort { $0.vote_average > $1.vote_average }
+        topMovies = Array(movies.prefix(20))
+    }
+
+
+    @IBAction func sortValueChanged(_ sender: UISegmentedControl) {
+        switch sortSegmentedControl.selectedSegmentIndex {
+        case 0:
+            sortMoviesByPopularity()
+        case 1:
+            sortMoviesByVoteCount()
+        case 2:
+            sortMoviesByVoteAverage()
+        default:
+            break
+        }
+        tableView.reloadData()
     }
     
     // MARK: - UITableViewDataSource methods
-        
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return topMovies.count
     }
-        
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
 
         if let label = cell.viewWithTag(1) as? UILabel {
-            let movie = topMovies[indexPath.row] // use topMovies here
+            let movie = topMovies[indexPath.row]
             label.text = movie.title
         }
 
