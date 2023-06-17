1ba697228b0c9c6ea33c5f2b3dccc78df7d9789d
blonded99 <didls2654@naver.com>
2023-06-18 07:25:36 +0900

    feat: change the right image of the movie appears on the search screen

--- movieinfo-iOS/SearchViewController.swift
+++ movieinfo-iOS/SearchViewController.swift
@@ -7,6 +7,25 @@
 
 import UIKit
 
+// Extend UIImageView to add load function
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self?.image = image
+                }
+            } else {
+                // In case image couldn't be loaded, set to a default image
+                DispatchQueue.main.async {
+                    self?.image = UIImage(named: "testImg") // Change "testImg" to your default image name
+                }
+            }
+        }
+    }
+}
+
+
 class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var searchTypeSegmentedControl: UISegmentedControl!
@@ -55,17 +74,23 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath)
+        let movie = searchResults[indexPath.row]
         
-        if let titleLabel = cell.viewWithTag(10) as? UILabel{
-            let movie = searchResults[indexPath.row]
+        if let titleLabel = cell.viewWithTag(10) as? UILabel {
             titleLabel.text = movie.title
-            if let posterImageView = cell.viewWithTag(11) as? UIImageView {
-                    posterImageView.image = UIImage(named: "testImg")
-                }
         }
         
+        if let posterImageView = cell.viewWithTag(11) as? UIImageView {
+            let baseUrl = "https://image.tmdb.org/t/p/w185"
+            let fullUrlString = baseUrl + movie.poster_path
+            if let fullUrl = URL(string: fullUrlString) {
+                posterImageView.load(url: fullUrl)
+            }
+        }
+            
         return cell
     }
+
     
     // MARK: - TableView Delegate Methods
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
